6de1ac3dc9338f6c8b5d1b92ee1bf15ec069837a
Nasrullah1999 <rahul.lawela92@gmail.com>
2025-03-04 11:44:53 +0800

    submit quiz1

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -18,21 +18,24 @@
 // algoritma
 //isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
 
-
-let nama = "Rahul", peran = "Penyihir";
-
-if (nama="") {
-    console.log("Nama Wajib Diisi")
-    return
-} else {
-    console.log("Pilih Peranmu untuk memulai game"+peran)
-    if (peran ="Ksatria" ){
-console.log("halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!")
-}else if(peran="Tabib"){
-console.log("halo Tabib ${nama} , kamu akan membantu temanmu yang terluka")
-}else if(peran="Penyihir"){
-console.log("halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!")
-}
-}
-
 //code disini gunakan console.log untuk outputnya
+
+let nama = "John" 
+let peran = 1 ;
+
+
+console.log("Selamat datang di Dunia Proxytia, ");
+if (nama === "" || peran === null) {
+  console.log("Nama harus diisi!");
+  console.log("Pilih Peranmu untuk memulai game!");
+} else if (peran === 1) {
+  console.log("Halo Ksatria "+nama+", kamu dapat menyerang dengan senjatamu!");
+} else if (peran === 2) {         
+    console.log("Halo Penyihir "+nama+", ciptakan keajaiban yang membantu kemenanganmu!");
+}
+else if (peran === 3) {
+    console.log("Halo Tabib "+nama+", kamu akan membantu temanmu yang terluka.");
+    }
+else {
+    console.log("kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada"); 
+}
